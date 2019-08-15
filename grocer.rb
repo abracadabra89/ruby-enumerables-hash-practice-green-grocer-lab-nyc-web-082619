78ab379392e6a11c6882be92aaee1d06b41df7ac
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 21:54:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,15 @@
 def consolidate_cart(cart)
-  # code here
- my_hash = {
-  :count => +1
-}
- cart.push(my_hash)
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
